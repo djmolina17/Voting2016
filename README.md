ae00ee1d1f1af589f3cc98219e0df2d1f9fe54fe
djmolina17 <djmolina@umich.edu>
2020-04-21 16:01:13 -0400

    Final Code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Josue Molina's Final Project SI507
+
+## Requirements
+- BeautifulSoup
+- Plotly
+- Requests
+
+## Instructions
+1. Run votingproject20.py
+2. Type in a state name at the terminal
+3. Open your browser and view results
+4. Type in another state or type "quit" to quit
--- votingproject20.py
+++ votingproject20.py
@@ -6,11 +6,25 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import sys
 import webbrowser 
+import sqlite3 
+import plotly.graph_objects as go
+from state_abbr import state_abbr as ab
 
 base_url = "https://www.vote.org" 
 
 def build_state_url_dict(cache={}):
+    '''  Creates a dictionary with key value pair of state and link
+    
+    Parameters
+    ----------
+    cache
+    Returns
+    -------
+    dictionary:
+    state name and link 
+    '''
     state_link ={}
     soup = make_url_request_using_cache( "https://www.vote.org/", cache)
     state_main__page_link_elements = soup.select(".ul-quicklinks li a")
@@ -21,7 +35,13 @@ def build_state_url_dict(cache={}):
     return state_link
 
 def build_state_object(cache, state_link):
-    '''
+    '''Creates a dictionary with key value pair of state name and state object
+    
+    Parameters
+    ----------
+    cache
+    state_link
+   
     Returns
     =============
     dict:
@@ -51,12 +71,28 @@ def build_state_object(cache, state_link):
 
 
 class State():
+    '''A state object
+    Attributes
+    ----------
+    name : string
+        The state name 
+    state_url: string
+        the state's link
+    covid_url: string
+        page of state-specific covid-19 information 
+    state_election_url: string
+        government election website that is state-specifc
+    short_name: string
+        state's abbreviation (sourced from state_abbr.py) 
+    '''
     def __init__(self, name, state_url, covid_url, state_election_url):
         self.state_url = state_url
         self.name = name
         self.covid_url = covid_url
         self.state_election_url = state_election_url
+        self.short_name = ab[name]
         
+    
     def __str__(self):
         return f'''{self.name}
 ==================
@@ -125,7 +161,84 @@ def make_url_request_using_cache(url, cache):
 
 
 
+def create_visualizations(chosen_state: State):
+    '''  Creates 4 bar graphs based on user's state input. Provides information about voting in 2016 and unemployment in 2016 as well.
+    
+    Parameters
+    ----------
+    chosen_state:State object
+    Returns
+    -------
+    None
+    '''
+    connection = sqlite3.connect("2016_POTUS_Election.db")
+    cursor = connection.cursor()
 
+    #1
+    query = "SELECT cand, votes FROM pres16results WHERE st = \"US\" ORDER BY votes DESC LIMIT 4"
+    result = cursor.execute(query).fetchall()
+    #print(result)
+    canditate_list = []
+    vote_general_count = []
+    for r in result:
+        canditate_list.append(r[0])
+        vote_general_count.append(r[1])
+    #print("canditate_list=", canditate_list)
+    #print("vote_general_count=", vote_general_count)
+    fig = go.Figure([go.Bar(x=canditate_list, y=vote_general_count)])
+    fig.update_layout(title='General Election Vote Count for POTUS Election 2016', xaxis={'title': 'Candidate Names'}, yaxis={'title': 'Popular Vote'})
+    fig.write_html("general-election.html", auto_open=True)
+
+    #2
+    query = f'SELECT cand, votes FROM pres16results WHERE st = "{chosen_state.short_name}" and county = "NA" ORDER BY votes DESC LIMIT 4'
+    result = cursor.execute(query).fetchall()
+    #print(result)
+    canditate_list = []
+    vote_state_count = []
+    for r in result:
+        canditate_list.append(r[0])
+        vote_state_count.append(r[1])
+    fig = go.Figure([go.Bar(x=canditate_list, y=vote_state_count)])
+    fig.update_layout(title=f'{chosen_state.name.title()} Election Vote Count for POTUS Election 2016', xaxis={'title': 'Candidate Names'}, yaxis={'title': 'Popular Vote'})
+    fig.write_html("state-election.html", auto_open=True)
+
+    # Unemployment dataset does not include these 3 states (and did not mention that in the description!)
+    if chosen_state.name in ["florida", "alaska", "georgia"]:
+        print("Sorry, unemployment data was not provided. Please check Bureau of Labor Statitics")
+        return
+
+    #3
+    query = f'SELECT Year, AVG(Rate) as avgRate FROM (SELECT * FROM Unemployment WHERE lower(State) = "{chosen_state.name}") GROUP BY year '
+    result = cursor.execute(query).fetchall()
+    #print("==========\n","query=", query, "result=", result)
+    year_list = []
+    state_unemployment_list = []
+    for r in result:
+        year_list.append(r[0])
+        state_unemployment_list.append(r[1])
+    fig = go.Figure([go.Bar(x=year_list, y=state_unemployment_list)])
+    fig.update_layout(title=f'{chosen_state.name.title()}\'s Average Unemployment Rate Across Time ', xaxis={'title': 'Year'}, yaxis={'title': 'Unemployment Average Rate'})
+    fig.write_html("state-unemployment.html", auto_open=True)
+
+    """
+    SELECT * FROM (SELECT County, AVG(Rate) as avgRate FROM (SELECT * FROM Unemployment WHERE Year = 2016 AND State = "Michigan") GROUP BY County ORDER BY avgRate DESC LIMIT 10) as countyRates JOIN (SELECT * FROM pres16results WHERE st = "MI") as presElectionState on countyRates.County = presElectionState.county
+    """
+
+    # 4 
+    query = f'SELECT County, AVG(Rate) as avgRate FROM (SELECT * FROM Unemployment WHERE Year = 2016 AND lower(State) = "{chosen_state.name}") GROUP BY County ORDER BY avgRate DESC LIMIT 10'
+    result = cursor.execute(query).fetchall()
+    #print("==========\n","query=", query, "result=", result)
+    county_list = []
+    unemployment_rate = []
+    for r in result:
+        county_list.append(r[0])
+        unemployment_rate.append(r[1])
+    fig = go.Figure([go.Bar(x=county_list, y=unemployment_rate)])
+    fig.update_layout(title=f'Top 10 Uemployment Rate By County in {chosen_state.name.title()} in 2016', xaxis={'title': 'County Names'}, yaxis={'title': 'Unemployment Rate'})
+    fig.write_html("county-unemployment.html", auto_open=True)
+
+
+    connection.close()
 
 
 
@@ -133,14 +246,22 @@ if __name__ == "__main__":
     print('Running votingproject20...')
     cache = load_cache()
     state_urls = build_state_url_dict(cache=cache)
-    print(state_urls)
-
+    #print(state_urls)
     state_objects_dict = build_state_object(cache,state_urls)
-    state_response = input('Type in a state: ')
-    chosen_state = state_objects_dict[state_response]
-    print(chosen_state)
-    webbrowser.open(chosen_state.state_url)
-    webbrowser.open(chosen_state.covid_url)
-    webbrowser.open(chosen_state.state_election_url)
+    while True:
+        state_response = input('Type in a state: ').lower()
+        if state_response == 'quit':
+            sys.exit()
+        while state_response not in state_objects_dict.keys():
+            print("Hey that's not real state!")
+            state_response = input('Type in a state: ').lower()
+            if state_response == 'quit':
+                sys.exit()
+        chosen_state = state_objects_dict[state_response]
+        print(chosen_state)
+        webbrowser.open(chosen_state.state_url)
+        webbrowser.open(chosen_state.covid_url)
+        webbrowser.open(chosen_state.state_election_url)
+        create_visualizations(chosen_state)
 
     
